aad428b5f92068e34ab88703e76d8f785ec93a83
Dieguidev <diegogaraycullas@gmail.com>
2023-10-11 07:00:17 -0500

    agregado pagination y modulo common

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true, //usualmente en produccion no se sincroniza
     }),
+    CommonModule,
   ],
 })
 export class AppModule {}
